8a9fcb062f7f71524ac0cc02c2977aaa888b742b
Rohit Sarkar <rohitsarkar5398@gmail.com>
2018-12-17 22:19:41 +0530

    tweak run script

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ password=$2
 
 if [ -z "$1" ] || [ -z "$2" ]
 then
-    echo "Usage: ./run.sh <username> <password>"
+    echo "Usage: yemit  <username> <password>"
     exit 1
 fi
 casperjs util.js --username=$username --password=$password
