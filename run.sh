f384283bce17465152d791a249079e54f5b3fa6f
Rohit Sarkar <rohitsarkar5398@gmail.com>
2018-12-17 22:01:18 +0530

    Modify run script

--- run.sh
+++ run.sh
@@ -3,5 +3,10 @@
 username=$1
 password=$2
 
+if [ -z "$1" ] || [ -z "$2" ]
+then
+    echo "Usage: ./run.sh <username> <password>"
+    exit 1
+fi
 casperjs util.js --username=$username --password=$password
 node index.js
